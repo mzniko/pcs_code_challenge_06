efbc2531b15e2f656119c14f08bd70bbc8fea77a
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-03-05 09:57:30 -0800

    added thanks page and tweaked tests

--- born/app.rb
+++ born/app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require 'pry'
 
 get '/' do
   erb :index
+end
+
+post '/thanks' do
+  @name = params["name"]
+  binding.pry
+  erb :thanks
 end
--- born/spec/born_spec.rb
+++ born/spec/born_spec.rb
@@ -1,16 +1,36 @@
 require 'minitest/autorun'
 require_relative 'spec_helper'
+require 'pry'
 
 describe 'index' do
-
-  it 'should render the index with a correct template and layout' do
+  it 'should render the index' do
     get '/'
-    assert_equal 'layout', last_response.header
+    assert last_response.ok?
   end
 
   it 'should have a title that says, Born Every Minute' do
     get '/'
     assert last_response.body.include?('<title>Born Every Minute</title>')
+  end
+end
+
+describe 'thanks' do
+  it 'should pull up the thanks page after a form submission' do
+    post '/thanks'
+    assert last_response.ok?
+  end
+
+  # it 'should contain form data' do
+  #   post '/thanks', 'name' => 'pedobear', 'mail' => 'pedo@bearmail.com',
+  #        'twitter' => '@pedobear', 'phone' => '123-456-7899'
+  #   assert request.form_data?
+  # end
+
+  it 'should reflect the correct name on form submission' do
+    post '/thanks', 'name' => 'pedobear'#, 'mail' => 'pedo@bearmail.com',
+         #'twitter' => '@pedobear', 'phone' => '123-456-7899'
+    assert last_response.body.include?('pedobear')
 
   end
 end
+
