93860c3e62068f3905aeba9c25433f7fc2177729
twurz <travis.wurzer@yahoo.com>
2014-03-08 10:08:46 -0800

    added to csv_spec file

--- born/spec/csv_spec.rb
+++ born/spec/csv_spec.rb
@@ -1,3 +1,4 @@
+require 'csv'
 require 'minitest/autorun'
 require_relative 'spec_helper'
 require 'pry'
@@ -5,13 +6,13 @@ require 'pry'
 #Tests for steps in CC-07, pulling suckers from CSV file.
 describe 'csv' do
   it 'should open the CSV file'
+    create_test_file
     get '/suckers'
+    assert last_response.body.include?('Roob')
 end
 
 def create_test_file
-  test = CSV.new("testfile")
-  testarray = ["", "Rae", "X.", "Roob", "MD","", "626", "343", "4393", "461", "Auer", "cale@franeckivandervort.com"]
-  test.generate_line(testarray)
-test
+  CSV.open("testfile.csv", 'w') do |test|
+    test << ["", "Rae", "X.", "Roob", "MD","", "626", "343", "4393", "461", "Auer", "cale@franeckivandervort.com"]
+  end
 end
-
