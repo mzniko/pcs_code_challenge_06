a0689b6bf7f8844a38081fadcca8928565e9fdea
twurz <travis.wurzer@yahoo.com>
2014-03-08 09:46:43 -0800

    added files for code challenge 07

---
+++ born/persistance.rb
@@ -0,0 +1,18 @@
+class Data
+  def initialize
+    #open CSV file
+  end
+
+  def parse
+    #parse the CSV
+    #output a hash
+  end
+
+  def goto
+    #access the file and return specific line
+  end
+
+  def close_file
+    #close the file
+  end
+end
--- born/spec/csv_spec.rb
+++ born/spec/csv_spec.rb
@@ -3,8 +3,15 @@ require_relative 'spec_helper'
 require 'pry'
 
 #Tests for steps in CC-07, pulling suckers from CSV file.
-describe 'csv' do 
-  it 'should open the file'
+describe 'csv' do
+  it 'should open the CSV file'
     get '/suckers'
-    
 end
+
+def create_test_file
+  test = CSV.new("testfile")
+  testarray = ["", "Rae", "X.", "Roob", "MD","", "626", "343", "4393", "461", "Auer", "cale@franeckivandervort.com"]
+  test.generate_line(testarray)
+test
+end
+
