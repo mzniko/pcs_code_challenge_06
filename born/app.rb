ffa3c916ad9f8cf265fba49896c87e55138717b3
Marko Nikolovski <mz.nikolovski@gmail.com>
2014-03-13 20:45:51 -0700

    implemented db, working on creation

--- born/app.rb
+++ born/app.rb
@@ -1,24 +1,62 @@
+require 'rubygems'
 require 'sinatra'
 require 'pry'
-require 'csv'
+require 'data_mapper'
 require_relative 'suckersinput.rb'
 require_relative 'persistance.rb'
 
 enable :sessions
+
+configure do
+  DataMapper.setup(:default, "sqlite3:///suckers.sqlite3")
+end
+
+class SuckerEntity
+  include DataMapper::Resource
+
+  property :id,             Serial
+  property :name_prefix,    String
+  property :first_name,     String
+  property :middle_name,    String
+  property :last_name,      String
+  property :suffix,         String
+  property :country_code,   String
+  property :area_code,      String
+  property :phone_prefix,   String
+  property :line,           String
+  property :extension,      String
+  property :twitter,        String
+  property :email,          String
+  property :created_at,     DateTime
+end
+
+DataMapper.finalize
 
 get '/' do
   erb :index
 end
 
 post '/suckers' do
-  s = Sucker.new
+  s = SuckerParse.new
   s.name = params[:name]
   s.phone = params[:phone]
   s.twitter = params[:twitter]
   s.email = params[:email]
-  CSV.open('people.csv', 'a') do |f|
-    f << s.csvstring
-  end
+
+  sucker = SuckerEntity.create( name_prefix: s.name[0],
+                                first_name: s.name[1],
+                                middle_name: s.name[2],
+                                last_name: s.name[3],
+                                suffix: s.name[4],
+                                country_code: s.phone[0],
+                                area_code: s.phone[1],
+                                phone_prefix: s.phone[2],
+                                line: s.phone[3],
+                                extension: s.phone[4],
+                                twitter: s.twitter,
+                                email: s.email,
+                                created_at: Time.now)
+
   session[:name] = params[:name]
   redirect to('/thanks')
 end
@@ -30,8 +68,8 @@ get '/thanks' do
 end
 
 get '/suckers' do
-  suckers = SuckerList.new
-  @suckers = suckers.suckers_display.to_a
+  @suckers = SuckerEntity.all
+  binding.pry
   session[:suckers_array] = @suckers
   erb :suckers, locals: { suck: @suckers }
 end
--- born/persistance.rb
+++ born/persistance.rb
@@ -1,20 +1,39 @@
 require 'pry'
-require_relative 'lib/concat_csv.rb'
-require 'csv'
+require 'data_mapper'
 
 # makes suckers happen
-class SuckerList
-  def suckers_display
-    display = []
-    CSV.foreach('people.csv') do |line|
-      new_sucker = ConcatCsv.new(line).combine
-      display << new_sucker
-    end
-    display
-  end
+class SuckerEntity
+  include DataMapper::Resource
 
-  def goto(line)
-    csv_line = CSV.readlines('people.csv')[line]
-    ConcatCsv.new(csv_line).combine
-  end
+  property :id,             Serial
+  property :name_prefix,    String
+  property :first_name,     String
+  property :middle_name,    String
+  property :last_name,      String
+  property :suffix,         String
+  property :country_code,   String
+  property :area_code,      String
+  property :phone_prefix,   String
+  property :line,           String
+  property :extension,      String
+  property :twitter,        String
+  property :email,          String
+  property :created_at,     DateTime
 end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+  # def suckers_display
+  #   display = []
+  #   CSV.foreach('people.csv') do |line|
+  #     new_sucker = ConcatCsv.new(line).combine
+  #     display << new_sucker
+  #   end
+  #   display
+  # end
+
+  # def goto(line)
+  #   csv_line = CSV.readlines('people.csv')[line]
+  #   ConcatCsv.new(csv_line).combine
+  # end
