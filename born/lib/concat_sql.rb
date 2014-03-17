0826d554a55e0f72a4c23c60713526dd93d6763c
twurz <travis.wurzer@yahoo.com>
2014-03-16 20:59:53 -0700

    working possible solution.

--- born/lib/concat_sql.rb
+++ born/lib/concat_sql.rb
@@ -1,40 +1,41 @@
 # concatanates a csv line into a suckers array
 require "pry"
-class ConcatSQL < Array
-  def initialize(sucker_line_array)
-    @sucker_line_array = sucker_line_array
-  end
-
-  def concat_entities(sucker_entities)
+module ConcatSQL
+  def ConcatSQL.concat_entities(sucker_entities)
     arry =[]
     sucker_entities.each do |sucker|
-      arry << ConcatSQL.new(sucker.attributes.values[1..12]).combine
+      arry << ConcatSQL.combine(sucker.attributes.values[1..12])
     end
-    binding.pry
+#    binding.pry
     arry
   end
 
-  def name_concat
+  def ConcatSQL.concat_entity(sucker)
+    ConcatSQL.combine(sucker.attributes.values[1..12])
+  end
+
+  def ConcatSQL.name_concat
     name = @sucker_line_array.shift(5)
     name.reject { |i| i.empty? }.join(' ')
   end
 
-  def phone_concat
+  def ConcatSQL.phone_concat
     phone_array = @sucker_line_array.shift(4)
     phone_string = phone_array.reject { |i| i.empty? }.join('-')
     phone_string << ' ' + @sucker_line_array.shift
     phone_string
   end
 
-  def twitter
+  def ConcatSQL.twitter
     @sucker_line_array.shift
   end
 
-  def email
+  def ConcatSQL.email
     @sucker_line_array.shift
   end
 
-  def combine
+  def ConcatSQL.combine(sucker_line_array)
+    @sucker_line_array = sucker_line_array
     combined_array = []
     combined_array << name_concat
     combined_array << phone_concat
